8907dcc575b07f3ed4649c302021fc1cb03b594c
Chanceler Shaffer <shafferchance@gmail.com>
2019-03-17 16:17:35 -0400

    Converting to tkinter for displaying image

--- Grayscale-Mixing/so_GrayMix.py
+++ Grayscale-Mixing/so_GrayMix.py
@@ -1,4 +1,20 @@
-from PIL import Image
-im = Image.open("lavee.jpg")
-im.show()
-input()
+from PIL import ImageTk, Image
+import tkinter as tk
+
+def main():
+    # Declaring image path
+    path = "lavee.jpg"
+
+    # Creating and adding elements to widget
+    wigdet = tk.Tk()
+    wigdet.title("Graymix")
+    wigdet.geometry("1024x720")
+    display = ImageTk.PhotoImage(Image.open(path))
+    panel = tk.Label(wigdet, image = display)
+
+    # Preparing final GUI and launching
+    panel.pack(side="bottom", fill="none",expand=False)
+    wigdet.mainloop()
+
+if __name__ == "__main__":
+    main()

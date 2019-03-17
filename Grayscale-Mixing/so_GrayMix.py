e617135f3a0cb86310175032f316432e6cb6f3a2
Chanceler Shaffer <shafferchance@gmail.com>
2019-03-17 17:58:48 -0400

    Beginning to add second window and logic

--- Grayscale-Mixing/so_GrayMix.py
+++ Grayscale-Mixing/so_GrayMix.py
@@ -1,20 +1,39 @@
-from PIL import ImageTk, Image
+from PIL import ImageColor, ImageTk, Image
 import tkinter as tk
+
+def grayMixer(image: ImageTk.Image):
+    # Color variables
+
+    # Creating mixer window
+    mixer = tk.Tk()
+    mixer.title("Grayscale Mixer")
+    mixer.geometry("250x250")
+
+    # Creating sliders for values
+    reds = tk.Scale(mixer)
 
 def main():
     # Declaring image path
     path = "lavee.jpg"
 
-    # Creating and adding elements to widget
-    wigdet = tk.Tk()
-    wigdet.title("Graymix")
-    wigdet.geometry("1024x720")
+    # Creating and configuring window
+    img = tk.Tk()
+    img.title("Example")
+    img.geometry("1024x720")
+    
+    # Creating and adding toolbar
+    tlbar = tk.Frame(img, bg="black", width="1024", height="100")
+    scaleMixer = tk.Button(tlbar, text="Grayscale Mixer",command="")
+    scaleMixer.pack(side="left", padx=2, pady=2)
+
+    # Creating and adding the image
     display = ImageTk.PhotoImage(Image.open(path))
-    panel = tk.Label(wigdet, image = display)
+    panel = tk.Label(tlbar, image = display)
+    panel.pack(side="bottom", fill="none", expand=False)
 
     # Preparing final GUI and launching
-    panel.pack(side="bottom", fill="none",expand=False)
-    wigdet.mainloop()
+    tlbar.pack(side="top")
+    img.mainloop()
 
 if __name__ == "__main__":
     main()

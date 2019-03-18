ea5c208bcdd301ca42f9b86cde240ec57e062631
Chanceler Shaffer <shafferchance@gmail.com>
2019-03-18 17:38:46 -0400

    Building RGB to HLS convo, need to fix

--- Grayscale-Mixing/so_GrayMix.py
+++ Grayscale-Mixing/so_GrayMix.py
@@ -1,26 +1,63 @@
 from PIL import ImageColor, ImageTk, Image
 import tkinter as tk
+import colorsys as cs
 
-def grayMixer():
-    # Color  = 0
-    magenta = 0
-    yellow = 0
-    cyan = 0
-    blue = 0
-    green = 0
+def rgbToHls(image: Image):
+    hlsImg = [[0]*(image.width) for _ in range(0, image.height)]
+    for x in range(0, image.height):
+        for y in range(0, image.width):
+            pixelData = image.getpixel((x,y)) / 255
+            hlsImg[x][y] = cs.rgb_to_hls(
+                            pixelData[0],
+                            pixelData[1],
+                            pixelData[2])
+    return hlsImg
 
+
+def grayMixer(image):
+    
     # Creating mixer window
     mixer = tk.Tk()
     mixer.title("Grayscale Mixer")
-    mixer.geometry("250x250")
+    mixer.geometry("250x400")
 
     # Creating sliders for values
-    reds = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Red")
-    magentas = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Magenta")
-    yellows = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Yellow")
-    cyans = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Cyans")
-    blues = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Blue")
-    greens = tk.Scale(mixer, from_ = 0, to = 255, orient="horizontal", label = "Green")
+    reds = tk.Scale(
+                mixer, 
+                from_ = 0, 
+                to = 255, 
+                orient = "horizontal",
+                label = "Red")
+    magentas = tk.Scale(
+                mixer, 
+                from_ = 0, 
+                to = 255, 
+                orient = "horizontal", 
+                label = "Magenta")
+    yellows = tk.Scale(
+                mixer, 
+                from_ = 0, 
+                to = 255, 
+                orient = "horizontal", 
+                label = "Yellow")
+    cyans = tk.Scale(
+                mixer, 
+                from_ = 0, 
+                to = 255, 
+                orient = "horizontal", 
+                label = "Cyans")
+    blues = tk.Scale(
+                mixer, 
+                from_ = 0, 
+                to = 255, 
+                orient = "horizontal", 
+                label = "Blue")
+    greens = tk.Scale(
+                mixer, 
+                from_ = 0, 
+                to = 255, 
+                orient="horizontal", 
+                label = "Green")
 
     # Adding sliders
     reds.pack()
@@ -40,15 +77,28 @@ def main():
     # Creating and configuring window
     img = tk.Tk()
     img.title("Example")
-    img.geometry("1024x720")
+    img.geometry("400x400")
+
+    # Opening Image
+    display = ImageTk.PhotoImage(Image.open(path))
     
     # Creating and adding toolbar
-    tlbar = tk.Frame(img, bg="black", width="1024", height="100")
-    scaleMixer = tk.Button(tlbar, text="Grayscale Mixer",command=grayMixer)
-    scaleMixer.pack(side="left", padx=2, pady=2)
+    tlbar = tk.Frame(
+                img, 
+                bg="black", 
+                width="400", 
+                height="100")
+    scaleMixer = tk.Button(
+                tlbar, 
+                text="Grayscale Mixer",
+                command=lambda img=display: grayMixer(img))
+    # Lambda necessary to pass function without improper handling
+    scaleMixer.pack(
+                side="left", 
+                padx=2, 
+                pady=2)
 
-    # Creating and adding the image
-    display = ImageTk.PhotoImage(Image.open(path))
+    # Adding the image
     panel = tk.Label(tlbar, image = display)
     panel.pack(side="bottom", fill="none", expand=False)
 

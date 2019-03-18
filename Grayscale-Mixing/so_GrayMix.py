507d7a8dda9326a8d5ba314592fd12962737620d
Chance Shaffer <shafferchance@gmail.com>
2019-03-18 08:08:13 -0400

    Finished adding basic gui for grayscale mixer

--- Grayscale-Mixing/so_GrayMix.py
+++ Grayscale-Mixing/so_GrayMix.py
@@ -1,8 +1,13 @@
 from PIL import ImageColor, ImageTk, Image
 import tkinter as tk
 
-def grayMixer(image: ImageTk.Image):
-    # Color variables
+def grayMixer():
+    # Color  = 0
+    magenta = 0
+    yellow = 0
+    cyan = 0
+    blue = 0
+    green = 0
 
     # Creating mixer window
     mixer = tk.Tk()
@@ -10,7 +15,23 @@ def grayMixer(image: ImageTk.Image):
     mixer.geometry("250x250")
 
     # Creating sliders for values
-    reds = tk.Scale(mixer)
+    reds = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Red")
+    magentas = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Magenta")
+    yellows = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Yellow")
+    cyans = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Cyans")
+    blues = tk.Scale(mixer, from_ = 0, to = 255, orient = "horizontal", label = "Blue")
+    greens = tk.Scale(mixer, from_ = 0, to = 255, orient="horizontal", label = "Green")
+
+    # Adding sliders
+    reds.pack()
+    magentas.pack()
+    yellows.pack()
+    cyans.pack()
+    blues.pack()
+    greens.pack()
+
+    # Preparing final GUI and launch
+    mixer.mainloop()
 
 def main():
     # Declaring image path
@@ -23,7 +44,7 @@ def main():
     
     # Creating and adding toolbar
     tlbar = tk.Frame(img, bg="black", width="1024", height="100")
-    scaleMixer = tk.Button(tlbar, text="Grayscale Mixer",command="")
+    scaleMixer = tk.Button(tlbar, text="Grayscale Mixer",command=grayMixer)
     scaleMixer.pack(side="left", padx=2, pady=2)
 
     # Creating and adding the image
